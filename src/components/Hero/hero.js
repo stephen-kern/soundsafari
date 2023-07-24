96da627d8565d9b313cefe5cac95a257c0dc0f28
stephen <94320530+stephen-kern@users.noreply.github.com>
2023-07-24 16:18:01 -0500

    hero paragraph update

--- src/components/Hero/hero.js
+++ src/components/Hero/hero.js
@@ -1,19 +1,16 @@
 import React from "react";
-import "./hero.css"
+import "./hero.css";
 
 function Hero() {
   return (
-
     <div className="content-container">
       <div className="hero-container">
         <h1>Welcome to Sound Safari</h1>
         <h3>Your Musical Oasis!</h3>
         <p>
-          Immerse yourself in the wonder of this web app,
-          providing a condensed version of your Spotify profile, showcasing your
-          most beloved artists. Experience tailor-made recommendations, inspired by your
-          recent music choices. Navigate the rhythm of life with Sound Safari,
-          where harmonies meet discovery!
+          Enjoy a condensed version of your Spotify profile, highlighting
+          beloved artists, and explore personalized recommendations based on
+          your recent music. Embrace the rhythm of life with Sound Safari!
         </p>
       </div>
     </div>
