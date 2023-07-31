e6bf84f160534b044d051966b0e27d75cd428df9
stephen <94320530+stephen-kern@users.noreply.github.com>
2023-07-31 17:52:50 -0500

    added hero styles.

--- src/components/Hero/hero.js
+++ src/components/Hero/hero.js
@@ -1,19 +1,18 @@
 import React from "react";
-import "./hero.css";
 
 function Hero() {
   return (
-    <div className="content-container">
-      <div className="hero-container">
+    <section className="hero-container">
+      <div className="hero-cta-container">
         <h1>Welcome to Sound Safari</h1>
         <h3>Your Musical Oasis!</h3>
-        <p>
+        <p className="hero-paragraph">
           Enjoy a condensed version of your Spotify profile, highlighting
           beloved artists, and explore personalized recommendations based on
           your recent music. Embrace the rhythm of life with Sound Safari!
         </p>
       </div>
-    </div>
+    </section>
   );
 }
 
