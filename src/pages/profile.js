8bd8838c7c5641880099b6f3bea322a253c0ab2a
stephen <94320530+stephen-kern@users.noreply.github.com>
2023-08-04 13:52:04 -0500

    styles and adding media queries

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -10,80 +10,78 @@ const Profile = ({ userData, accessToken, recentData, relatedData }) => {
 
   return (
     <>
-      {/* <div className="profile-container"> */}
-        <div className="profile-card">
-          <div className="profile-header">
-            <div className="profile-items">
-              <img
-                className="profile-avatar"
-                alt="Profile Avatar"
-                src={userData.images[0].url}
-              />
-              <div className="profile-icons-info">
-                <PiUsersBold className="profile-icons" />
-                <p>{userData.followers.total}</p>
-              </div>
+      <div className="profile-card">
+        <div className="profile-header">
+          <div className="profile-items">
+            <img
+              className="profile-avatar"
+              alt="Profile Avatar"
+              src={userData.images[0].url}
+            />
+            <div className="profile-icons-info">
+              <PiUsersBold className="profile-icons" />
+              <p>{userData.followers.total}</p>
             </div>
           </div>
-
-          <h1>Welcome {userData.display_name}!</h1>
-
-          <Tabs onTabClick={handleTabClick} activeTab={activeTab} />
         </div>
 
-        <div className="container">
-          {activeTab === "Recent" && recentData && (
-            <>
-              <h2>Your Recent Artists:</h2>
-              <div className="recent-container">
-                {recentData.items.map((item, index) => (
-                  <div key={index} className="artist-cards">
-                    {" "}
-                    <img
-                      className="avatar"
-                      src={item.images[0]?.url}
-                      alt={item.name}
-                    />{" "}
-                    <a
-                      href={item.external_urls.spotify}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      {item.name}
-                    </a>
-                  </div>
-                ))}
-              </div>
-            </>
-          )}
-        </div>
+        <h1>Welcome {userData.display_name}!</h1>
 
-        <div className="container">
-          {activeTab === "Related" && relatedData && (
-            <>
-              <h2>Artists You May Enjoy</h2>
-              <div className="related-container">
-                {relatedData.tracks.map((track, index) => (
-                  <div key={index} className="artist-cards">
-                    <img
-                      className="avatar"
-                      src={track.album.images[0]?.url}
-                      alt={track.nam}
-                    />{" "}
-                    <a
-                      href={track.artists[0].external_urls.spotify}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      {track.artists.map((artist) => artist.name).join(", ")}
-                    </a>
-                  </div>
-                ))}
-              </div>
-            </>
-          )}
+        <Tabs onTabClick={handleTabClick} activeTab={activeTab} />
+      </div>
+
+      {activeTab === "Recent" && recentData && (
+        <div>
+          <div className="container">
+            <h2>Your Recent Artists</h2>
+            <div className="recent-container">
+              {recentData.items.map((item, index) => (
+                <div key={index} className="artist-cards">
+                  {" "}
+                  <img
+                    className="avatar"
+                    src={item.images[0]?.url}
+                    alt={item.name}
+                  />{" "}
+                  <a
+                    href={item.external_urls.spotify}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {item.name}
+                  </a>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
-      {/* </div> */}
+      )}
+
+      {activeTab === "Related" && relatedData && (
+        <div className="flex-center">
+          <div className="container">
+            <h2>Artists You May Enjoy</h2>
+            <div className="related-container">
+              {relatedData.tracks.map((track, index) => (
+                <div key={index} className="artist-cards">
+                  <img
+                    className="avatar"
+                    src={track.album.images[0]?.url}
+                    alt={track.nam}
+                  />{" "}
+                  <a
+                    href={track.artists[0].external_urls.spotify}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {track.artists.map((artist) => artist.name).join(", ")}
+                  </a>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      )}
     </>
   );
 };
